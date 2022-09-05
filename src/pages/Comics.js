d678948f81287ad4718fe932398c5cc0c1d5eaf0
priscillia <priscillia.g.pro@outlook.fr>
2022-09-05 12:57:02 +0200

    add data json Waiting for the repair of the corns error

--- src/pages/Comics.js
+++ src/pages/Comics.js
@@ -1,59 +1,63 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import { HashLink } from "react-router-hash-link";
+// import axios from "axios";
+// import React, { useEffect, useState } from "react";
+// import { HashLink } from "react-router-hash-link";
 import Hulk from "../assets/img/hulk-5959620_960_720.png";
 import CardComponent from "../components/Card";
 // import SearchBar from "../components/SearchBar";
-import Spinner from "../components/Spinner";
+// import Spinner from "../components/Spinner";
+
+//********************************************* */
+// En attendant la réparation de l'erreur CORS
+import comics from "../assets/data/comics.json";
+//********************************************* */
 
 function Comics() {
-  const [data, setData] = useState();
-  const [isLoading, setIsLoading] = useState(true);
-  const [page, setPage] = useState(1);
-  // const [input, setInput] = useState("");
+  // const [data, setData] = useState();
+  // const [isLoading, setIsLoading] = useState(true);
+  // const [page, setPage] = useState(1);
+  // // const [input, setInput] = useState("");
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          `https://marvel-students.herokuapp.com/comics?page=${page}`
-        );
-        setData(response.data);
-        setIsLoading(false);
-      } catch (error) {
-        console.log("console log error =>", error);
-      }
-    };
-    fetchData();
-  }, [page]);
-  return isLoading === true ? (
-    <Spinner title="axiosError-repair in progress " />
-  ) : (
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await axios.get(
+  //         `https://marvel-students.herokuapp.com/comics?page=${page}`
+  //       );
+  //       setData(response.data);
+  //       setIsLoading(false);
+  //     } catch (error) {
+  //       console.log("console log error =>", error);
+  //     }
+  //   };
+  //   fetchData();
+  // }, [page]);
+  // return isLoading === true ? (
+  //   <Spinner title="axiosError-repair in progress " />
+  // ) :
+  return (
     <>
       <div className="min-h-screen bg-gradient-to-r from-green-400 to-blue-500 flex justify-center items-center py-20">
         <div className="flex flex-wrap w-full justify-center">
           {" "}
-          {data.results.slice(0, 15)?.map((comics) => {
+          {comics.results.slice(0, 15)?.map((comics) => {
             const imagePath = comics.thumbnail.path + "/portrait_uncanny.jpg";
             return (
-              <div key={comics._id}>
-                <CardComponent
-                  style={{
-                    backgroundImage: `url(${imagePath ? imagePath : Hulk})`,
-                  }}
-                  title={comics.title}
-                  description={
-                    comics.description
-                      ? comics.description
-                      : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit voluptate sed doloribus, tenetur porro veniam numquam,pariatur sapiente eius eos fugit possimus omnis. "
-                  }
-                />
-              </div>
+              <CardComponent
+                style={{
+                  backgroundImage: `url(${imagePath ? imagePath : Hulk})`,
+                }}
+                title={comics.title}
+                description={
+                  comics.description
+                    ? comics.description
+                    : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit voluptate sed doloribus, tenetur porro veniam numquam,pariatur sapiente eius eos fugit possimus omnis. "
+                }
+              />
             );
           })}
         </div>
       </div>
-      <div className=" bg-gradient-to-r from-green-400 to-blue-500 text-center pb-5">
+      {/* <div className=" bg-gradient-to-r from-green-400 to-blue-500 text-center pb-5">
         <button
           className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l"
           onClick={() => setPage(page - 1)}
@@ -71,7 +75,7 @@ function Comics() {
         >
           Next
         </button>
-      </div>
+      </div> */}
     </>
   );
 }
